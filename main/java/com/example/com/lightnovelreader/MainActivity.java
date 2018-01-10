0747e82042c10680ecb5bd2e40d981b0d4f3b959
Juan José Ñustes <nustesj@gmail.com>
2018-01-09 19:02:37 -0500

    Prueba de Layouts

---
+++ main/java/com/example/com/lightnovelreader/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.com.lightnovelreader;
+
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Intent invoker;
+    //FragmentManager fragmentManager;
+    //FragmentTransaction fragmentTransaction;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+                    android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    HomeFragment homeFragment = new HomeFragment();
+                    fragmentTransaction.add(R.id.container, homeFragment);
+                    fragmentTransaction.commit();
+                    return true;
+                case R.id.navigation_dashboard:
+                    //intent = new Intent();
+                    return true;
+                case R.id.navigation_notifications:
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
